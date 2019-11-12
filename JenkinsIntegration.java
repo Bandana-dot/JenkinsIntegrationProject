2fe9cae72ed0202ab068c72777494e376725f310
Bandana <bandanajoshi12@gmail.com>
2019-11-12 14:58:56 -0500

    Added Jenkins Integration Project

---
+++ JenkinsIntegration.java
@@ -0,0 +1,11 @@
+package com.bandana.jenkinsIntgration;
+
+public class JenkinsIntegration {
+
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++)
+			System.out.println("Output :  This Project is for Jenkins Integration with GIT.\n");
+
+	}
+
+}
